fea1dae6416156189ea28f4933784378afc6fc6e
DDilocker <84706575+DDilocker@users.noreply.github.com>
2024-02-03 18:10:11 +0100

    Update BatShutoffList.h

--- include/BatShutoffList.h
+++ include/BatShutoffList.h
@@ -36,18 +36,18 @@
 // clang-format off
 //                    ID                Type    Mode        Min     Min_Release Max     Max_Release
 #define BatShutoffTriggerList \
-    BatShutoffTrigger(AD_C1,            Ovp,    MaxCompare, 0,      0,          3.9,    3.65) \
-    BatShutoffTrigger(AD_C2,            Ovp,    MaxCompare, 0,      0,          3.9,    3.65) \
-    BatShutoffTrigger(AD_C3,            Ovp,    MaxCompare, 0,      0,          3.9,    3.65) \
-    BatShutoffTrigger(AD_C4,            Ovp,    MaxCompare, 0,      0,          3.9,    3.65) \
-    BatShutoffTrigger(AD_C1,            Lvp,    MinCompare, 2.8,    3.2,        0,      0) \
-    BatShutoffTrigger(AD_C2,            Lvp,    MinCompare, 2.8,    3.2,        0,      0) \
-    BatShutoffTrigger(AD_C3,            Lvp,    MinCompare, 2.8,    3.2,        0,      0) \
-    BatShutoffTrigger(AD_C4,            Lvp,    MinCompare, 2.8,    3.2,        0,      0) \
-    BatShutoffTrigger(AD_C1,            Final,  AllCompare, 2.7,    2.8,        4.0,    4.0) \
-    BatShutoffTrigger(AD_C2,            Final,  AllCompare, 2.7,    2.8,        4.0,    4.0) \
-    BatShutoffTrigger(AD_C3,            Final,  AllCompare, 2.7,    2.8,        4.0,    4.0) \
-    BatShutoffTrigger(AD_C4,            Final,  AllCompare, 2.7,    2.8,        4.0,    4.0)
+    BatShutoffTrigger(AD_C1,            Ovp,    MaxCompare, 0,      0,          3.75,    3.60) \
+    BatShutoffTrigger(AD_C2,            Ovp,    MaxCompare, 0,      0,          3.75,    3.60) \
+    BatShutoffTrigger(AD_C3,            Ovp,    MaxCompare, 0,      0,          3.75,    3.60) \
+    BatShutoffTrigger(AD_C4,            Ovp,    MaxCompare, 0,      0,          3.75,    3.60) \
+    BatShutoffTrigger(AD_C1,            Lvp,    MinCompare, 2.9,    3.2,        0,      0) \
+    BatShutoffTrigger(AD_C2,            Lvp,    MinCompare, 2.9,    3.2,        0,      0) \
+    BatShutoffTrigger(AD_C3,            Lvp,    MinCompare, 2.9,    3.2,        0,      0) \
+    BatShutoffTrigger(AD_C4,            Lvp,    MinCompare, 2.9,    3.2,        0,      0) \
+    BatShutoffTrigger(AD_C1,            Final,  AllCompare, 2.8,    3.2,        3.85,    4.0) \
+    BatShutoffTrigger(AD_C2,            Final,  AllCompare, 2.8,    3.2,        3.85,    4.0) \
+    BatShutoffTrigger(AD_C3,            Final,  AllCompare, 2.8,    3.2,        3.85,    4.0) \
+    BatShutoffTrigger(AD_C4,            Final,  AllCompare, 2.8,    3.2,        3.85,    4.0)
 // clang-format on
 
 /**
